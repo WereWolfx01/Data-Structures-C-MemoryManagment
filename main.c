95257279d40d79049a3eb60dd91aa9e4e603c567
Aeman Abdulmuhssen <aabdulmu@uoguelph.ca>
2019-09-13 11:53:00 -0400

    initial commit

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 
 int main() {
 
-  printf("Hello World\n");
+  printf("sizeof( struct ds_counts_struct ) = %ld\n", sizeof( struct ds_counts_struct ));
+  printf("sizeof( struct ds_counts_struct ) = %d\n", sizeof( struct ds_counts_struct ));
+  printf("sizeof( struct ds_blocks_struct ) = %ld\n", sizeof( struct ds_blocks_struct ));
+  printf("sizeof( struct ds_file_struct ) = %ld\n", sizeof( struct ds_file_struct ));
   return 0;
 }
