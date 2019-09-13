03cc873886e157e70946a62646d9f243b3bb2eca
Aeman Abdulmuhssen <aabdulmu@uoguelph.ca>
2019-09-13 00:45:35 -0400

    initial commit

---
+++ ds_memory.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+#define MAX_BLOCKS 4096
+
+struct ds_counts_struct {
+  int reads;
+  int writes;
+};
+
+struct ds_blocks_struct {
+  long start;
+  long length;
+  char alloced;
+};
+
+struct ds_file_struct {
+  FILE *fp;
+  struct ds_blocks_struct block[MAX_BLOCKS];
+};
+
+struct ds_file_struct ds_file;
+struct ds_counts_struct ds_counts;
+
+int ds_create( char *filename, long size );
+int ds_init( char *filename );
+int ds_malloc( long amount );
+void ds_free( long start );
+void *ds_read( void *ptr, long start, long bytes );
+long ds_write( long start, void *ptr, long bytes );
+int ds_finish();
---
+++ makefile
@@ -0,0 +1,2 @@
+main: ds_memory.h ds_memory.c main.c
+	gcc -ansi -pedantic -Wall ds_memory.h ds_memory.c main.c -o main
